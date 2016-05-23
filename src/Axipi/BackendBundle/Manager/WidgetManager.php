b938fc6381625ac7a518229ef00298374fa5b3b5
Stéphane Dion <divers@sdion.net>
2016-05-23 22:54:26 +0200

    -

--- src/Axipi/BackendBundle/Manager/WidgetManager.php
+++ src/Axipi/BackendBundle/Manager/WidgetManager.php
@@ -52,7 +52,7 @@ class WidgetManager extends AbstractManager
                             @unlink('uploads/'.$data->getAttribute('image'));
                         }
                         $data->setAttribute($key, $attribute->getClientOriginalName());
-                        $data->setAttribute($key.'_mime', $attribute->getClientMimeType());
+                        $data->setAttribute($key.'_mime', $attribute->getMimeType());
                         $data->setAttribute($key.'_size', $attribute->getClientSize());
                         $attribute->move('uploads', $attribute->getClientOriginalName());
                     }
