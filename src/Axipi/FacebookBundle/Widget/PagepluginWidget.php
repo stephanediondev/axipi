80d3597d5d41114fc2658d12ea2491e07cccd7ee
Stéphane Dion <divers@sdion.net>
2016-06-05 08:21:26 +0200

    -

---
+++ src/Axipi/FacebookBundle/Widget/PagepluginWidget.php
@@ -0,0 +1,25 @@
+<?php
+namespace Axipi\FacebookBundle\Widget;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+use Axipi\CoreBundle\Widget\AbstractWidget;
+
+class PagepluginWidget extends AbstractWidget
+{
+    public function getWidget(Request $request, $widget, $page)
+    {
+        $parameters = new ParameterBag();
+        $parameters->set('request', $request);
+        $parameters->set('widget', $widget);
+        $parameters->set('page', $page);
+
+        if($widget->getTemplate()) {
+            $template = $widget->getTemplate();
+        } else {
+            $template = $widget->getComponent()->getTemplate();
+        }
+        return $this->renderWidget($template, $parameters->all());
+    }
+}
