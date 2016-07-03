8ce0210204b46b8ac8ebea963129eca881c90c72
Stéphane Dion <divers@sdion.net>
2016-07-03 08:36:59 +0200

    -

---
+++ src/Axipi/ContactBundle/Widget/FormWidget.php
@@ -0,0 +1,43 @@
+<?php
+namespace Axipi\ContactBundle\Widget;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+use Axipi\CoreBundle\Widget\AbstractWidget;
+use Axipi\ContactBundle\Form\Type\MessageType;
+
+class FormWidget extends AbstractWidget
+{
+    public function getWidget($parameters)
+    {
+        $form = $this->createForm(MessageType::class, null, []);
+        $form->handleRequest($parameters->get('request'));
+
+        if($form->isSubmitted()) {
+            if($form->isValid()) {
+                $data = $form->getData();
+
+                $message = \Swift_Message::newInstance();
+                $message->setSubject($parameters->get('widget')->getTitle());
+                $message->setFrom(array($parameters->get('widget')->getAttribute('recipient_email') => $parameters->get('widget')->getAttribute('recipient_name')));
+                $message->setTo(array($parameters->get('widget')->getAttribute('recipient_email') => $parameters->get('widget')->getAttribute('recipient_name')));
+                $message->setReplyTo(array($data['email'] => $data['author']));
+                $message->setBody(strip_tags($data['message']));
+
+                $this->get('mailer')->send($message);
+
+                $this->addFlash('success', 'created');
+            }
+        }
+
+        $parameters->set('form', $form->createView());
+
+        if($parameters->get('widget')->getTemplate()) {
+            $template = $parameters->get('widget')->getTemplate();
+        } else {
+            $template = $parameters->get('widget')->getComponent()->getTemplate();
+        }
+        return $this->renderWidget($template, $parameters->all());
+    }
+}
